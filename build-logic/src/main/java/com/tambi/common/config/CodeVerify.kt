d46a14cacb15c37d8843219a123de30e14284de4
최재영 <jychoi@huray.net>
2023-12-18 17:57:58 +0900

    [SETTING] add detekt config file and apply to code verify config

--- build-logic/src/main/java/com/tambi/common/config/CodeVerify.kt
+++ build-logic/src/main/java/com/tambi/common/config/CodeVerify.kt
@@ -2,14 +2,21 @@ package com.tambi.common.config
 
 import com.tambi.extensions.versionCatalog
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
 internal fun Project.configureCodeVerify() {
     with(pluginManager) {
         apply("org.jlleitschuh.gradle.ktlint")
+        apply("io.gitlab.arturbosch.detekt")
     }
 
     dependencies {
         add("ktlint", versionCatalog.findLibrary("ktlint-compose-gradlePlugin").get())
+    }
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        allRules = false
     }
 }
